b4bf4fd254037d67ae2b8ba6a3ef9562d186c812
helpfulrobot <chris@silverstripe.com>
2015-12-18 10:58:12 +1300

    Converted to PSR-2

--- code/FAIconizedObject.php
+++ code/FAIconizedObject.php
@@ -1,14 +1,13 @@
 <?php
 class FAIconizedObject extends DataExtension
 {
-	private static $db = array(
-			'FAIcon' => 'FAIcon', 
-		);
-	
-	public function updateCMSFields (FieldList $fields)
-	{
-		$iconField = new FAIconField('FAIcon');
-		$fields->addFieldToTab('Root.Main', $iconField);
-	}
+    private static $db = array(
+            'FAIcon' => 'FAIcon',
+        );
+    
+    public function updateCMSFields(FieldList $fields)
+    {
+        $iconField = new FAIconField('FAIcon');
+        $fields->addFieldToTab('Root.Main', $iconField);
+    }
 }
-?>
--- code/forms/FAIconField.php
+++ code/forms/FAIconField.php
@@ -1,20 +1,19 @@
 <?php
 class FAIconField extends DropdownField
 {
-	private static $ICONS = array(
-			'person' => 'User', 
-			'persons' => 'Users'
-		);
-	
-	public function __construct ($name, $title=null, $source=array(), $value='', $form=null, $emptyString=null)
-	{
-		parent::__construct($name, $title, $source, $value, $form, $emptyString);
-		$this->setIconsByType(FAIcon::ALL);
-	}
-	
-	public function setIconsByType ($iconType)
-	{
-		$this->setSource(FAIcon::config()->get($iconType));
-	}
+    private static $ICONS = array(
+            'person' => 'User',
+            'persons' => 'Users'
+        );
+    
+    public function __construct($name, $title=null, $source=array(), $value='', $form=null, $emptyString=null)
+    {
+        parent::__construct($name, $title, $source, $value, $form, $emptyString);
+        $this->setIconsByType(FAIcon::ALL);
+    }
+    
+    public function setIconsByType($iconType)
+    {
+        $this->setSource(FAIcon::config()->get($iconType));
+    }
 }
-?>
